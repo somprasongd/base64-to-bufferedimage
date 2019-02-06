37bc6449168adf2b086e3034007abbea9bbcc21e
Somprasong Damyos <somprasong@hospital-os.com>
2019-02-06 14:38:26 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const decodeBase64Image = dataString => {
-  const response = {};
+  if (dataString === null || dataString.trim() === "") {
+    return { error: new Error("Invalid input string") };
+  }
+  const data = {};
   const dataPureBase64 = dataString.split("\n").join("");
   const matches = dataPureBase64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-  console.log(dataPureBase64);
 
   if (
     matches === null &&
     Buffer.from(dataPureBase64, "base64").toString("base64") === dataPureBase64
   ) {
-    response.type = null;
-    response.data = Buffer.from(dataPureBase64, "base64");
-    return response;
+    data.type = null;
+    data.buffer = Buffer.from(dataPureBase64, "base64");
+    return { error: undefined, data };
   }
 
   if (matches === null || matches.length !== 3) {
-    return new Error("Invalid input string");
+    return { error: new Error("Invalid input string") };
   }
-  response.type = matches[1];
-  response.data = Buffer.from(matches[2], "base64");
-  return response;
+  data.type = matches[1];
+  data.buffer = Buffer.from(matches[2], "base64");
+  return { error: undefined, data };
 };
 
 module.exports = {
